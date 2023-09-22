b9b30936f7087bb084a81239da7b53aaf2474bc8
Asiak <asiakemp24@gmail.com>
2023-09-21 21:54:52 -0400

    created resolvers.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,51 @@
+// import the User and Workout models
+const User = require('../models/User');
+const Workout = require('../models/Workout');
+
+const resolvers = {
+    Query: {
+        user: async (_, args, context) => {
+            // check if authenticated
+            if (!context.user) {
+                throw new Error('Not logged in');
+            }
+
+            // return user
+            return context.user;
+        },
+        // resolver for workouts query
+        workouts: async (_, args, context) => {
+            // check if authenticated
+            if(!context.user) {
+                throw a Error('Not logged in');
+            }
+            // fetch and return workouts
+            const workouts = await Workout.find({ user: context.user.id });
+            return workouts;
+        },
+    },
+    Mutation: {
+        // resolver for addWorkout mutation
+        addWorkout: async (_, args, context) => {
+            // check if authenticated
+            if(!context.user) {
+                throw new Error('Not logged in');
+            }
+            // create workout instance
+            const newWorkout = new Workout({
+                user: context.user.id,
+                exercise: args.input.exercise,
+                duration: args.input.duration,
+                reps: args.input.reps,
+                sets: args.input.sets,
+            });
+            // save workout
+            const savedWorkout = await newWorkout.save();
+
+            // return workout
+            return savedWorkout;
+        },
+    },
+};
+
+module.exports = resolvers;
