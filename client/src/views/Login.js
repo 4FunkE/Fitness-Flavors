2820c00b18e959b8ce4b2283de0a4150eb1c33eb
KEINance <keinance2@gmail.com>
2023-09-23 11:46:18 -0400

    className fixed

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -6,21 +6,21 @@ import  { SignUp } from '../views/SignUp'
 export default function makeNewUser() {
   // JSX code for login form
   const login = (
-    <div id="login" class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
+    <div id="login" className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-6">
         <h2>Welcome back!</h2>
-        <form class="login-form">
-          <div class="form-group">
+        <form className="login-form">
+          <div className="form-group">
             <label for="email-login">Email:</label>
-            <input type="text" class="form-control" id="email-login" />
+            <input type="text" className="form-control" id="email-login" />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="password-login">Password:</label>
-            <input type="password" class="form-control" id="password-login" />
+            <input type="password" className="form-control" id="password-login" />
           </div>
-          <div class="form-group">
-            <button type="submit" class="btn btn-primary">Login</button>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">Login</button>
           </div>
         </form>
         <p>Let's get an account set up for you! <a href="/SignUp"> Signup </a></p>
