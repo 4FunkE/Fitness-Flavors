fdd6049fdca2c2084e3bc2589b85dc06849339c2
KEINance <keinance2@gmail.com>
2023-09-25 17:09:49 -0400

    login and sign up with render test code

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,22 +1,50 @@
 //sign up for app
 import React, { useState } from 'react';
-import './App.css';
+// import './App.css';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/Mutations.js'
+
 
 export default function Signup() {
-  // JSX code for login form
-  const signUp = (
+  const [formData, setFormData] = useState({ username: '', password: '' })
+  const [ addUser ] = useMutation(ADD_USER);
+  const handleInputChange = (event) => {
+    const {name, value} = event.target;
+    setFormData ({ ...formData, [name]: value });
+  }
+
+  const handleInputSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      console.log('test submitted', formData);
+      //inseert sign up mutation here
+      const { data } = await addUser({ variables: { ...formData } });
+      // front end auth token here
+    } catch(error) {
+      console.error('error')
+    }
+
+  }
+
+  // code for login form
+  return (
     <div className="container">
     <div>
       <div>
         <h2>Let's Get Started!</h2>
-        <form className="login-form">
-          <div className="form-group">
+        <form className="login-form" onSubmit={handleInputSubmit}>
+          {/* <div className="form-group">
             <label for="email-login">Email:</label>
             <input type="text" className="form-control" id="email-login" />
+          </div> */}
+          
+          <div className="form-group">
+            <label for="username-login">Username:</label>
+            <input type="text" className="form-control" id="username-login" name='username' value={formData.username} onChange={handleInputChange} />
           </div>
           <div className="form-group">
             <label for="password-login">Password:</label>
-            <input type="password" className="form-control" id="password-login" />
+            <input type="password" className="form-control" name="password" value={formData.password} onChange={handleInputChange} id="password-login" />
           </div>
           <div className="form-group">
             <button type="submit" className="btn">Login</button>
