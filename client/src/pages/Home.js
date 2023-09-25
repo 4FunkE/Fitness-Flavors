243d1d04f135041511de54dc206c3ffa0ca856c4
TheWiseSailor <rudasilljay@gmail.com>
2023-09-24 21:49:31 -0400

    Overhauled the home.js to give a more realistic approach compaired that crappy one made previously

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,70 +1,107 @@
 import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
+import { GET_MESSAGE, UPDATE_MESSAGE } from "./graphql";
 import "../styles/Home.css";
-import { GET_MESSAGE, UPDATE_MESSAGE } from "./graphql"; // Import your GraphQL queries and mutations
 
-function Home() {
-  // Define a state variable to store the updated message
-  const [newMessage, setNewMessage] = useState("");
+function HomePage() {
+  // Define state for the mobile menu
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  // Define a query to fetch the current message
+  // Define  GraphQL query
   const { loading, error, data } = useQuery(GET_MESSAGE);
 
-  // Define a mutation to update the message
+  // Define  GraphQL mutation
   const [updateMessage] = useMutation(UPDATE_MESSAGE);
 
-  // Function to handle the form submission and update the message
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Call the updateMessage mutation to update the message
-    updateMessage({ variables: { newMessage } });
+  // Handle mobile menu toggle
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
   return (
-    <div className="homepage">
-      {/* Navbar */}
-      <nav className="navbar bg-blue-500 p-4">{/* ... Navbar content */}</nav>
-
-      {/* Hero Section */}
-      <section className="hero bg-gray-100 py-12">
-        <div className="hero-content text-center">
-          <h1 className="animated fadeInDownBig text-3xl font-bold text-blue-800 mb-4">
-            {data.message}
-          </h1>
-          <p className="animated fadeInUpBig text-gray-600 mb-4">Motto</p>
-          <form onSubmit={handleSubmit} className="animated fadeInLeftBig">
-            <input
-              type="text"
-              placeholder="Enter a new message"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              className="border p-2 rounded-md mr-2 focus:outline-none focus:border-blue-500"
-            />
+    <div className="bg-gray-100 font-sans">
+      <nav className="bg-blue-500 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl text-white font-semibold">Fitness Hub</h1>
+          {/* Hamburger Menu for Mobile */}
+          <div className="sm:hidden">
             <button
-              type="submit"
-              className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none"
+              onClick={toggleMobileMenu}
+              className="text-white hover:text-gray-300 focus:outline-none"
             >
-              Update Message
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
             </button>
-          </form>
+          </div>
+          {/* Desktop Menu */}
+          <ul className="hidden sm:flex space-x-4">
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Classes
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Trainers
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-      </section>
+      </nav>
 
-      {/* Additional Content */}
-      <section className="additional-content">
-        {/* more sections/content here with animations */}
-      </section>
+      {/* Mobile Menu Overlay */}
+      {isMobileMenuOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-blue-500 bg-opacity-90 z-50 transform translate-x-full transition-transform ease-in-out duration-300">
+          <ul className="text-white text-center mt-16">
+            <li>
+              <a href="#" className="block py-4 px-6 text-xl hover:bg-blue-600">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-4 px-6 text-xl hover:bg-blue-600">
+                Classes
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-4 px-6 text-xl hover:bg-blue-600">
+                Trainers
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-4 px-6 text-xl hover:bg-blue-600">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
 
-      {/* Footer */}
-      <footer className="footer bg-gray-200 p-4 text-center">
-        {/* footer content */}
-      </footer>
+      {/* Rest of  content */}
     </div>
   );
 }
 
-export default Home;
+export default HomePage;
