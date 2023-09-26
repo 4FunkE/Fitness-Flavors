aef0fdea56db4cdd0f05b81b42fe1d5e4686fe3d
TheWiseSailor <rudasilljay@gmail.com>
2023-09-26 14:50:30 -0400

    I fixed the error where I had issues with tailwind not working so I had to use CDN link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,42 +16,48 @@ function HomePage() {
   };
 
   // Use the in-view hook to detect when the sliding images section is in view
-  useEffect(() => {
-    const link = document.createElement("link");
-    link.href =
-      "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css";
-    link.rel = "stylesheet";
-    document.head.appendChild(link);
-
-    // Clean up the link element when the component unmounts
-    return () => {
-      document.head.removeChild(link);
-    };
-  }, []);
 
   return (
     <div>
-      <div className="container mx-auto mt-8">
-        <p>Your content goes here.</p>
-      </div>
-
       {/* Sliding Images Section */}
-      <div className="container mx-auto mt-8">
-        <div className="flex flex-wrap">
+      <div className="container mx-auto mt-8 text-center ">
+        <div className="flex flex-wrap ">
           {/* Apply animation classes */}
-          <div className="w-64 md:w-1/5 animate__animated animate__fadeInLeft img1">
+          <div className="w-64 md:w-1/5 animate__animated animate__slideInLeft img1">
             <img src={image1} alt="Image 1" />
           </div>
-          <div className="w-full md:w-1/5 animate__animated animate__fadeInRight img2">
+          <div className="w-64 md:w-1/5 animate__animated animate__slideInRight img2">
             <img src={image2} alt="Image 2" />
           </div>
-          <div className="w-full md:w-1/5 animate__animated animate__fadeInLeft img3">
+          <div className="w-64 md:w-1/5 animate__animated animate__slideInLeft img3">
             <img src={image3} alt="Image 3" />
           </div>
-          <div className="w-full md:w-1/5 animate__animated animate__fadeInRight img4">
+          <div className="w-64 md:w-1/5 animate__animated animate__slideInRight img4">
             <img src={image4} alt="Image 4" />
           </div>
-          {/* Add more images as needed */}
+        </div>
+      </div>
+      {/* About Section */}
+      <div className="container mx-auto mt-8">
+        <h3 className="text-2xl font-semibold mb-4 text-center">About Us</h3>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius
+          tristique urna, id consectetur libero tincidunt eget. Nullam efficitur
+          aliquet lectus, at tristique dolor tempor non. Vivamus id justo at
+          quam fermentum bibendum.
+        </p>
+      </div>
+      {/* Contact Section */}
+      <div className="bg-blue-500 py-64 text-white text-center">
+        <div className="container mx-auto">
+          <h3 className="text-2xl font-semibold mb-4">Contact Us</h3>
+          <p>Have questions or need assistance? Contact us today!</p>
+          <button className="bg-white text-blue-500 rounded-full px-6 py-2 mt-4 hover:bg-blue-600 hover:text-white">
+            Get in Touch
+          </button>
+          <form className="mt-4">
+            {/* Add form fields and animation classes */}
+          </form>
         </div>
       </div>
     </div>
