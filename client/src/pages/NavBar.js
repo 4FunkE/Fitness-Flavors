f8d3fce537c9b909f25c3ac73f9affdb63067b2d
4FunkE <4FunkEcodes@gmail.com>
2023-09-26 18:01:13 -0400

    debugging return in navbar

--- client/src/pages/NavBar.js
+++ client/src/pages/NavBar.js
@@ -1,18 +1,18 @@
 // nav for app
-import React, { useState } from "react";
+import React from "react";
 // import './App.css';
 
 export default function NavBar() {
+  return (
     <div id="navBar" className="container">
-
-    <div id="navBarText">
-      <ul>
-        <li>Profile</li>
-        <li>SignIn</li>
-        <li>Profile</li>
-        {/* <li>Nutrition COMING SOON</li> */}
-      </ul>
+      <div id="navBarText">
+        <ul>
+          <li>Profile</li>
+          <li>SignIn</li>
+          <li>Profile</li>
+          {/* <li>Nutrition COMING SOON</li> */}
+        </ul>
+      </div>
     </div>
-    
-  </div>
-};
+  );
+}
