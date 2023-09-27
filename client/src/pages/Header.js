7fad6b2865dc62412a850d5dc93977a1569de1b6
KEINance <keinance2@gmail.com>
2023-09-27 19:17:59 -0400

    test

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -13,3 +13,39 @@ export default function Header() {
   </div>
   );
 };
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+// import './App.css';
+
+export default function Header() {
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  if (searchInput.length > 0) {
+    exercise.filter((exercise) => {
+      return exercise.name.match(searchInput);
+    });
+  }
+
+  return (
+    <div id="header" className="container">
+      {/* make search bar */}
+      <div id="headerText">
+        <h2>Fitness Flavors</h2>
+        <NavBar></NavBar>
+        <input
+          type="text"
+          placeholder="Search here"
+          onChange={handleChange}
+          value={searchInput}
+        />
+      </div>
+      <div id="headerText">
+        <h2>Fitness Flavors</h2>
+      <NavBar />
+    </div>
+  </div>
+  );
+};
