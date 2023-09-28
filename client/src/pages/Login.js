7e7a56eb6fa33e3caf4d8eec74c438f6848895e8
Asiak <asiakemp24@gmail.com>
2023-09-27 21:15:38 -0400

    uncommented code

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,101 +1,101 @@
-// //login page for app
-// import React, { useState } from "react";
-// import '../styles/Login.css'; // Import your CSS file for styling
+//login page for app
+import React, { useState } from "react";
+import '../styles/Login.css'; // Import your CSS file for styling
 
-// import { Link } from 'react-router-dom';
-// import { useMutation } from '@apollo/client';
-// // import { LOGIN_USER } from '../utils/Mutations';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/Mutations';
 
-// import Auth from '../utils/auth';
+import Auth from '../utils/auth';
 
-// const Login = (props) => {
-//   const [formData, setFormData] = useState({ username: '', password: '' })
-//   const [login, { error, data }] = useMutation(LOGIN_USER);
+const Login = (props) => {
+  const [formData, setFormData] = useState({ username: '', password: '' })
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
-//   // update state based on form input changes
-//   const handleInputChange = (event) => {
-//     const {name, value} = event.target;
+  // update state based on form input changes
+  const handleInputChange = (event) => {
+    const {name, value} = event.target;
     
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   }
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
 
-//   // submit form
-//   const handleInputSubmit = async (event) => {
-//     event.preventDefault()
-//     console.log(formData);
-//     try {
-//       console.log('test submitted', formData);
-//       const { data } = await login({
-//         variables: { ...formData }
-//       });
+  // submit form
+  const handleInputSubmit = async (event) => {
+    event.preventDefault()
+    console.log(formData);
+    try {
+      console.log('test submitted', formData);
+      const { data } = await login({
+        variables: { ...formData }
+      });
 
-//       Auth.login(data.login.token);
-//     } catch (error) {
-//       console.error('error login.js, line 37')
-//     }
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.error('error login.js, line 37')
+    }
 
-//     // clear form values
-//     setFormData({
-//       username: '',
-//       password: '',
-//     });
-//   };
+    // clear form values
+    setFormData({
+      username: '',
+      password: '',
+    });
+  };
   
 
-// //code for login form
-// return (
-//   <div id="login" className="container">
-//     <div className="row justify-content-center">
-//       <div className="col-md-6">
-//         <h2 className='loginH'>Welcome back!</h2>
-//         <div className="card-body">
-//             {data ? (
-//               <p>
-//                 Success! You may now head{' '}
-//                 <Link to="/">back to the homepage.</Link>
-//               </p>
-//             ) : (
-//               <form className="login-form" onSubmit={handleInputSubmit}>
-//                 <input
-//                   className="form-input"
-//                   placeholder="Your Username:"
-//                   name="username"
-//                   type="string"
-//                   value={formData.username}
-//                   onChange={handleInputChange}
-//                 />
-//                 <input
-//                   className="form-input"
-//                   placeholder="******"
-//                   name="password"
-//                   type="password"
-//                   value={formData.password}
-//                   onChange={handleInputChange}
-//                 />
-//                 <button
-//                   className="btn btn-block btn-info"
-//                   style={{ cursor: 'pointer' }}
-//                   type="submit"
-//                 >
-//                   Submit
-//                 </button>
-//               </form>
-//             )}
+//code for login form
+return (
+  <div id="login" className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        <h2 className='loginH'>Welcome back!</h2>
+        <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form className="login-form" onSubmit={handleInputSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your Username:"
+                  name="username"
+                  type="string"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+                <button
+                  className="btn btn-block btn-info"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
 
-//             {error && (
-//               <div className="my-3 p-3 bg-danger text-white">
-//                 {error.message}
-//               </div>
-//             )}
-//           <p>Let's get an account set up for you! <a href="/SignUp"> Signup </a></p>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-//   );
-// };
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          <p>Let's get an account set up for you! <a href="/SignUp"> Signup </a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+};
 
-// export default Login;
+export default Login;
