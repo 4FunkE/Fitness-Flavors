6ece549f0c8191e909e71e1524b317d5b2f401c0
TheWiseSailor <rudasilljay@gmail.com>
2023-09-29 14:14:31 -0400

    got the scroll animation to work, now I will just tidy up the styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, lazy, Suspense } from "react";
+import React, { useState, useEffect } from "react";
 import "animate.css"; // Import animate.css
 import image1 from "../components/images/mail.jpg";
 import homeimg from "../components/images/homeimg.jpg";
@@ -7,6 +7,9 @@ import "../index.css";
 import workout1 from "../components/images/gigachad.jpg";
 import workout2 from "../components/images/yoga.jpg";
 import workout3 from "../components/images/workout3.jpg";
+
+// Import the ScrollAnimatedSection component
+import ScrollAnimatedSection from "./ScrollAnimatedSection";
 
 function HomePage() {
   // Define state for the cards data
@@ -19,6 +22,7 @@ function HomePage() {
       .then((data) => setCardsData(data))
       .catch((error) => console.error(error));
   }, []);
+
   // Function to fetch data from your API
   const fetchDataFromAPI = async () => {
     try {
@@ -52,206 +56,215 @@ function HomePage() {
   return (
     <div>
       {/* Hero Section */}
-      <div className="bg-custom-dark-blue py-16 md:py-32 text-white text-center relative">
-        <div
-          className="absolute inset-0 h-full w-full bg-cover bg-center"
-          style={{ backgroundImage: `url(${homeimg})` }}
-        ></div>
+      <ScrollAnimatedSection>
+        <div className="bg-custom-dark-blue py-16 md:py-32 text-white text-center relative">
+          <div
+            className="absolute inset-0 h-full w-full bg-cover bg-center"
+            style={{ backgroundImage: `url(${homeimg})` }}
+          ></div>
 
-        <div className="container mx-auto relative">
-          <div className="container mx-auto text-center text-white pt-96 h-screen">
-            {" "}
-            <div className="animate__animated animate__slideInLeft custom-slide-in-left">
-              <h1 className=" animate__animated animate__fadeInUp text-3xl md:text-5xl font-bold ">
-                Achieve Your Fitness Goals
-              </h1>
-              <p className="animate__animated animate__fadeInUp animate__delay-1s text-xl md:text-2xl mt-4 animate__delay-1s animate__slideInRight">
-                Join us and transform your body!
-              </p>
-              <a
-                href="#"
-                className="mt-8 inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 md:px-6 rounded-full animate__animated animate__fadeInUp animate__delay-2s infinite-pulse"
-              >
-                Get Started
-              </a>
+          <div className="container mx-auto relative">
+            <div className="container mx-auto text-center text-white pt-96 h-screen">
+              {" "}
+              <div className="animate__animated animate__slideInLeft custom-slide-in-left">
+                <h1 className=" animate__animated animate__fadeInUp text-3xl md:text-5xl font-bold ">
+                  Achieve Your Fitness Goals
+                </h1>
+                <p className="animate__animated animate__fadeInUp animate__delay-1s text-xl md:text-2xl mt-4 animate__delay-1s animate__slideInRight">
+                  Join us and transform your body!
+                </p>
+                <a
+                  href="#"
+                  className="mt-8 inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 md:px-6 rounded-full animate__animated animate__fadeInUp animate__delay-2s infinite-pulse"
+                >
+                  Get Started
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </ScrollAnimatedSection>
 
       {/* Cards Section */}
-      <div className="bg-custom-primary text-center ">
-        <div className="flex flex-wrap">{renderCards()}</div>
-      </div>
+      <ScrollAnimatedSection>
+        <div className="bg-custom-primary text-center ">
+          <div className="flex flex-wrap">{renderCards()}</div>
+        </div>
+      </ScrollAnimatedSection>
 
       {/* About Section */}
-      {/* About Section */}
-      <div className="bg-custom-secondary">
-        <div className="container mx-auto text-center">
-          <h3 className="text-2xl md:text-3xl font-semibold mb-4 text-center pt-24 pb-8">
-            About Us
-          </h3>
-          <p className="text-center mt-4 text-lg md:text-xl font-semibold pb-16">
-            "Harnessing the Energy of Fitness to Ignite Passion for a Balanced
-            Life."
-          </p>
-          <div class="flex justify-center  space-x-4 pb-24">
-            <img
-              src={workout1}
-              alt="Workout 1"
-              class="ml-4 w-1/4 rounded-xl animate__animated animate__slideInLeft"
-            />
-            <img
-              src={workout2}
-              alt="Workout 2"
-              class="w-1/4 rounded-xl animate__animated animate__slideInUp"
-            />
-            <img
-              src={workout3}
-              alt="Workout 3"
-              class="w-1/4 rounded-xl animate__animated animate__slideInRight"
-            />
+      <ScrollAnimatedSection>
+        <div className="bg-custom-secondary">
+          <div className="container mx-auto text-center">
+            <h3 className="text-2xl md:text-3xl font-semibold mb-4 text-center pt-24 pb-8">
+              About Us
+            </h3>
+            <p className="text-center mt-4 text-lg md:text-xl font-semibold pb-16">
+              "Harnessing the Energy of Fitness to Ignite Passion for a Balanced
+              Life."
+            </p>
+            <div class="flex justify-center  space-x-4 pb-24">
+              <img
+                src={workout1}
+                alt="Workout 1"
+                class="ml-4 w-1/4 rounded-xl animate__animated animate__slideInLeft"
+              />
+              <img
+                src={workout2}
+                alt="Workout 2"
+                class="w-1/4 rounded-xl animate__animated animate__slideInUp"
+              />
+              <img
+                src={workout3}
+                alt="Workout 3"
+                class="w-1/4 rounded-xl animate__animated animate__slideInRight"
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </ScrollAnimatedSection>
+
       {/* Our Classes Section */}
-      <section className="bg-custom-dark-blue  py-8 md:py-16 ">
-        <div className="container mx-auto text-center pb-20">
-          <h2 className="text-white text-xl md:text-3xl font-semibold pb-8">
-            Our Classes
-          </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 mt-4 md:mt-8">
-            {/* Class Card 1 */}
-            <div className="animate__animated animate__slideInLeft">
-              <div className="bg-custom-secondary rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300">
-                <h3 className=" text-lg md:text-xl font-semibold">Yoga</h3>
-                <p className="text-gray-600 mt-2 pb-6">
-                  Improve flexibility and find inner peace with our yoga
-                  classes.
-                </p>
+      <ScrollAnimatedSection>
+        <section className="bg-custom-dark-blue  py-8 md:py-16 ">
+          <div className="container mx-auto text-center pb-20">
+            <h2 className="text-white text-xl md:text-3xl font-semibold pb-8">
+              Our Classes
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 mt-4 md:mt-8">
+              {/* Class Card 1 */}
+              <div className="animate__animated animate__slideInLeft">
+                <div className="bg-custom-secondary rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300">
+                  <h3 className=" text-lg md:text-xl font-semibold">Yoga</h3>
+                  <p className="text-gray-600 mt-2 pb-6">
+                    Improve flexibility and find inner peace with our yoga
+                    classes.
+                  </p>
+                </div>
               </div>
-            </div>
-            {/* Class Card 2 */}
-            <div className="animate__animated animate__slideInUp">
-              <div className="bg-custom-secondary rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300">
-                <h3 className="text-lg md:text-xl font-semibold">
-                  Strength Training
-                </h3>
-                <p className="text-gray-600 mt-2 ">
-                  Build muscle and increase your strength with our strength
-                  training sessions.
-                </p>
+              {/* Class Card 2 */}
+              <div className="animate__animated animate__slideInUp">
+                <div className="bg-custom-secondary rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300">
+                  <h3 className="text-lg md:text-xl font-semibold">
+                    Strength Training
+                  </h3>
+                  <p className="text-gray-600 mt-2 ">
+                    Build muscle and increase your strength with our strength
+                    training sessions.
+                  </p>
+                </div>
               </div>
-            </div>
-            {/* Class Card 3 */}
-            <div className="animate__animated animate__slideInRight">
-              <div className="bg-custom-secondary rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300">
-                <h3 className="text-lg md:text-xl font-semibold">Cardio</h3>
-                <p className="text-gray-600 mt-2 pb-6">
-                  Get your heart rate up and burn calories in our cardio
-                  classes.
-                </p>
+              {/* Class Card 3 */}
+              <div className="animate__animated animate__slideInRight">
+                <div className="bg-custom-secondary rounded-lg p-4 md:p-6 shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300">
+                  <h3 className="text-lg md:text-xl font-semibold">Cardio</h3>
+                  <p className="text-gray-600 mt-2 pb-6">
+                    Get your heart rate up and burn calories in our cardio
+                    classes.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </ScrollAnimatedSection>
 
       {/* Contact Section */}
-
-      <div className="bg-custom-accent py-16 md:py-32 text-white text-center">
-        <div className="container mx-auto flex flex-col md:flex-row items-center">
-          <div className="w-full md:w-1/2 p-4 md:p-8 md:pl-32">
-            <img
-              src={image1}
-              alt="Image 1"
-              className="max-w-full h-auto md:pt-16"
-            />
-          </div>
-          <div className="w-full md:w-1/2 p-4 md:p-8">
-            <div className="w-full md:w-96 mx-auto">
-              <h3 className="text-lg md:text-2xl font-semibold mb-4">
-                Contact Us
-              </h3>
-              <p className="text-white">
-                Have questions or need assistance? Contact us today!
-              </p>
-              <form className="mt-4">
-                <div className="mb-4">
-                  <label
-                    className="block text-left text-white mb-2"
-                    htmlFor="name"
+      <ScrollAnimatedSection>
+        <div className="bg-custom-accent py-16 md:py-32 text-white text-center">
+          <div className="container mx-auto flex flex-col md:flex-row items-center">
+            <div className="w-full md:w-1/2 p-4 md:p-8 md:pl-32">
+              <img
+                src={image1}
+                alt="Image 1"
+                class="max-w-full h-auto md:pt-16 move-up-down"
+              />
+            </div>
+            <div className="w-full md:w-1/2 p-4 md:p-8">
+              <div className="w-full md:w-96 mx-auto">
+                <h3 className="text-lg md:text-2xl font-semibold mb-4">
+                  Contact Us
+                </h3>
+                <p className="text-white">
+                  Have questions or need assistance? Contact us today!
+                </p>
+                <form className="mt-4">
+                  <div className="mb-4">
+                    <label
+                      className="block text-left text-white mb-2"
+                      htmlFor="name"
+                    >
+                      Name
+                    </label>
+                    <input
+                      className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder="Your Name"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-left text-white mb-2"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Your Email"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-left text-white mb-2"
+                      htmlFor="subject"
+                    >
+                      Subject
+                    </label>
+                    <input
+                      className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
+                      type="text"
+                      id="subject"
+                      name="subject"
+                      placeholder="Subject"
+                      required
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="block text-left text-white mb-2"
+                      htmlFor="message"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
+                      id="message"
+                      name="message"
+                      rows="6"
+                      placeholder="Your Message"
+                      required
+                    ></textarea>
+                  </div>
+                  <button
+                    type="submit"
+                    className="text-white bg-blue-500 rounded-full px-4 md:px-6 py-2 hover:bg-green-600 hover:text-white mt-4 animate__animated animate__fadeInUp animate__delay-2s infinite-pulse"
                   >
-                    Name
-                  </label>
-                  <input
-                    className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Your Name"
-                    required
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="block text-left text-white mb-2"
-                    htmlFor="email"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Your Email"
-                    required
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="block text-left text-white mb-2"
-                    htmlFor="subject"
-                  >
-                    Subject
-                  </label>
-                  <input
-                    className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
-                    type="text"
-                    id="subject"
-                    name="subject"
-                    placeholder="Subject"
-                    required
-                  />
-                </div>
-                <div className="mb-6">
-                  <label
-                    className="block text-left text-white mb-2"
-                    htmlFor="message"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    className="w-full border-2 border-blue-300 rounded-lg py-2 px-3"
-                    id="message"
-                    name="message"
-                    rows="6"
-                    placeholder="Your Message"
-                    required
-                  ></textarea>
-                </div>
-                <button
-                  type="submit"
-                  className="bg-white text-blue-500 rounded-full px-4 md:px-6 py-2 hover:bg-green-600 hover:text-white mt-4"
-                >
-                  Submit
-                </button>
-              </form>
+                    Submit
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </ScrollAnimatedSection>
     </div>
   );
 }
---
+++ client/src/pages/ScrollAnimatedSection.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const ScrollAnimatedSection = ({ children }) => {
+  const sectionRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (sectionRef.current) {
+        const rect = sectionRef.current.getBoundingClientRect();
+        const windowHeight =
+          window.innerHeight || document.documentElement.clientHeight;
+
+        if (rect.top < windowHeight * 0.75 && rect.bottom >= 0) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll(); // Check initial state
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <section
+      ref={sectionRef}
+      className={`bg-custom-dark-blue py-8 md:py-16 ${
+        isVisible ? "animate__animated animate__fadeIn" : ""
+      }`}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default ScrollAnimatedSection;
