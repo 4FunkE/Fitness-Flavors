f0ee101c9ca4f2036fe3d71055a47cb8815f614b
4FunkE <4FunkEcodes@gmail.com>
2023-10-02 16:56:08 -0400

    eslint

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 // Connect to the MongoDB database using the provided MONGODB_URI or a local default URL
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fitnessFlavors_Db',
-{
-    useNewUrlParser: true,  // Use the new URL parser
-    useUnifiedTopology: true,  // Use the new Server Discovery and Monitoring engine
-}
+  {
+    useNewUrlParser: true, // Use the new URL parser
+    useUnifiedTopology: true, // Use the new Server Discovery and Monitoring engine
+  }
 );
 
 module.exports = mongoose.connect();
