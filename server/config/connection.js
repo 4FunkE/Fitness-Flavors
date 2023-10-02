338acdc4422f40ebba3587fb38826ebc91b63d43
4FunkE <4FunkEcodes@gmail.com>
2023-10-02 16:55:29 -0400

    deploying mongoose to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database using the provided MONGODB_URI or a local default URL
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fitness-flavors',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fitnessFlavors_Db',
 {
     useNewUrlParser: true,  // Use the new URL parser
     useUnifiedTopology: true,  // Use the new Server Discovery and Monitoring engine
 }
 );
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connect();
 
